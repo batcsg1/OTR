03bca31f536ceb633a3164d635621cc6f0108562
batcsg1 <batcsg1@student.op.ac.nz>
2026-01-01 21:22:10 +1300

    refactor: added config object for db.js

--- database/access/src/db.js
+++ database/access/src/db.js
@@ -2,38 +2,32 @@ import mariadb from "mariadb";
 import dotenv from "dotenv";
 dotenv.config({ path: "../docker/.env" });
 
+const config = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_DATABASE_USER ?? "root",
+  password: process.env.DB_ROOT_PASSWORD,
+  database: process.env.DB_DATABASE_NAME,
+};
+
 export const connection = async () => {
-    try {
-        const db = await mariadb.createConnection({
-            host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
-            user: process.env.DB_DATABASE_USER ?? "root",
-            password: process.env.DB_ROOT_PASSWORD,
-            database: process.env.DB_DATABASE_NAME
-        });
-        console.log("Database connection established");
-        return db;
-    } catch (error) {
-        console.log("Error connecting to the database:", error);
-        return error;
-    }
-}
+  try {
+    const db = await mariadb.createConnection(config);
+    console.log("Database connection established");
+    return db;
+  } catch (error) {
+    console.log("Error connecting to the database:", error);
+    return error;
+  }
+};
 
 export const pool = async () => {
-    try {
-        const db = mariadb.createPool({
-            host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
-            user: process.env.DB_DATABASE_USER ?? "root",
-            password: process.env.DB_ROOT_PASSWORD,
-            database: process.env.DB_DATABASE_NAME
-        });
-        console.log("Database pool established");
-        return db;
-    } catch (error) {
-        console.log("Error establishing pool to the database:", error);
-        return error;
-    }
-}
-
-
+  try {
+    const db = mariadb.createPool(config);
+    console.log("Database pool established");
+    return db;
+  } catch (error) {
+    console.log("Error establishing pool to the database:", error);
+    return error;
+  }
+};
